81e795ef0d667163aeabde672bdef765d3c2ad6a
Jimmy Cai <jimmycai2266@gmail.com>
2025-05-27 16:34:24 -0400

    chore: set up backend & frontend using spring initializr and vite

---
+++ backend/src/test/java/com/linkedin/Linkedin/LinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.Linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
